811d9bc9c0322e0e72fbaec0a9c1bed144e9cb64
MohamadAhmadi <mohammadahmadidezhnet@gmail.com>
2021-05-29 17:38:43 +0430

    changing celery features

--- DjangoEcommerceWebsite/one.py
+++ DjangoEcommerceWebsite/one.py
@@ -1,8 +1,10 @@
 from celery import Celery
 from time import sleep
 
+app = Celery('one', backend='rpc://', broker='amqp://localhost')
 
-app= Celery('one', broker='amqp://localhost')
+
+@app.task
 def add(x, y):
     sleep(5)
     return x + y
